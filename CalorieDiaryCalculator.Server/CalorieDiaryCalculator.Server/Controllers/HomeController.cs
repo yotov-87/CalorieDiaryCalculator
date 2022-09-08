3c96639eac72cafa3c29b4a488f4555ab8a15868
dimitar.yotov <dimitar.yotov@blu11.com>
2022-09-08 20:44:30 +0300

    removed unnecessary views and etc...

--- CalorieDiaryCalculator.Server/CalorieDiaryCalculator.Server/Controllers/HomeController.cs
+++ CalorieDiaryCalculator.Server/CalorieDiaryCalculator.Server/Controllers/HomeController.cs
@@ -1,26 +1,14 @@
-﻿using CalorieDiaryCalculator.Server.Models;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
 namespace CalorieDiaryCalculator.Server.Controllers {
-    public class HomeController : Controller {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger) {
-            _logger = logger;
-        }
-
-        public IActionResult Index() {
-            return View();
-        }
-
-        public IActionResult Privacy() {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error() {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    [ApiController]
+    public class HomeController : ControllerBase {
+        //[Authorize]
+        [Route("[controller]")]
+        public IActionResult Get() {
+            return Ok("Works!!!");
         }
     }
 }
